ae7d6b51be4cb0e5b1e3df5bad36f0a35fac68e9
Kender ROMAIN <kenderromain@gmail.com>
2023-03-21 18:22:28 -0400

    Add elements to Navbar in style them.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import styles from '../styles/Navbar.module.css'
+import Link from 'next/link';
+import React from 'react';
+import styles from '../styles/Navbar.module.css';
 const Navbar = () => {
   return (
-    <div className={styles.container}>Navbar</div>
-  )
-}
+    <div className={styles.container}>
+     <Link className={styles.logo} href="/">KR</Link>
+      <nav className={styles.nav}>
+        <ul className={styles.menuLinks}>
+          <li><Link href="/WebDev">WEB DEVELOPMENT</Link></li>
+          <li><Link href="/MobileDev">MOBILE DEVELOPMENT</Link></li>
+          <li><Link href="/Blog">BLOG</Link></li>
+          <li><Link href="/Contact">CONTACT</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
