33d13e6f1bc734600ed68b9ca3fc3ba2ef70f43b
Kender ROMAIN <kenderromain@gmail.com>
2023-03-22 17:06:10 -0400

    Create Contact page.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,7 @@
 import leaderboard from '../../public/assets/projects/leaderboard-screenshot.png';
-import bookstore from '../../public/assets/projects/Book Store.png';
+import bookstore from '../../public/assets/projects/book-store.png';
 import todoReact from '../../public/assets/projects/TO DO LIST-REACT.png';
+import countries from '../../public/assets/projects/countries.png';
 
 export const data = [
   {
@@ -11,17 +12,24 @@ export const data = [
     demo: 'https://leaderboard-kfp4.onrender.com/',
   },
   {
-    id: 1,
+    id: 2,
     img: bookstore,
-    desc: 'Leaderboard is a project allows users to provide their score and display them in the leaderboard of the game. Build with HTML,CSS,JavaScript,API, and Webpack. This project uses Gitflow.',
+    desc: 'This project allows the user to add, display, and remove a selected display book. Built with React and use Redux as state manager.',
     src: 'https://github.com/kensteph/book-store',
     demo: 'https://bookstore-cms-xxgs.onrender.com/',
   },
   {
-    id: 1,
+    id: 3,
     img: todoReact,
-    desc: 'Leaderboard is a project allows users to provide their score and display them in the leaderboard of the game. Build with HTML,CSS,JavaScript,API, and Webpack. This project uses Gitflow.',
+    desc: 'Web-based application built using the React JavaScript library, designed for managing and keeping track of tasks or to-do lists. It includes features such as adding new tasks, marking completed tasks.',
     src: 'https://github.com/kensteph/todo-react-app',
     demo: 'https://todolist-react-cn3p.onrender.com/',
+  },
+  {
+    id: 4,
+    img: countries,
+    desc: 'A web application that allows a user to filter a list of countries and get more details regarding this country. Built with React, Redux and also consume an API.',
+    src: 'https://github.com/kensteph/countries-details',
+    demo: 'https://countries-details.onrender.com/',
   },
 ];
